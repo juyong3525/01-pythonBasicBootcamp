26d2e8825a557adf76dd9be89573f0c77bd8477c
Juyong Hong <juyong3525@gmail.com>
2021-01-17 13:58:12 +0900

    crawling inner crawling

--- test.py
+++ test.py
@@ -5,10 +5,17 @@ URL = "http://corners.gmarket.co.kr/Bestsellers?viewType=G&groupCode=G06"
 
 res = requests.get(URL)
 if res.status_code == 200:
-    soup = BeautifulSoup(res.text, 'html.parser')
+    soup = BeautifulSoup(res.content, 'html.parser')
     cards = soup.select('.best-list')[1].select('ul > li')
 
     for index, card in enumerate(cards):
         title = card.select_one('.itemname')
         price = card.select_one('.s-price > strong')
-        print(index + 1, title.get_text(), price.get_text())
+
+        res_info = requests.get(title['href'])
+        if res_info.status_code == 200:
+            soup_info = BeautifulSoup(res_info.content, 'html.parser')
+            provider_info = soup_info.select_one('.text__seller > a')
+
+            print(index + 1, title.get_text(),
+                  price.get_text(), provider_info.get_text())
